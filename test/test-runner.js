ef2294356a7f427fe156e96cd6f07a3e0d9e9ffe
Viktor Estrada <vitekes@gmail.com>
2025-06-22 14:55:17 +0300

    refactor test runner

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,2 +1,12 @@
-const { execSync } = require('child_process');
-execSync('npx hardhat test', { stdio: 'inherit' });
+const { spawn } = require('child_process');
+const path = require('path');
+
+// Path to local Hardhat binary for cross-platform support
+const bin = path.resolve(__dirname, '../node_modules/.bin/hardhat');
+const cmd = process.platform === 'win32' ? `${bin}.cmd` : bin;
+
+const args = ['test', ...process.argv.slice(2)];
+
+const child = spawn(cmd, args, { stdio: 'inherit' });
+child.on('exit', code => process.exit(code ?? 1));
+
