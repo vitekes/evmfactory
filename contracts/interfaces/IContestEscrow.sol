9ebd21c30185fecc85c3c61cce9a13b751242a6b
Viktor Estrada <vitekes@gmail.com>
2025-06-25 22:57:26 +0300

    Remove contest v1 contracts and tests

--- contracts/interfaces/IContestEscrow.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-import {PrizeInfo} from '../modules/contests/shared/PrizeInfo.sol';
-
-interface IContestEscrow {
-    function addPrizes(PrizeInfo[] calldata prizes) external;
-    function finalize(address[] calldata winners) external;
-}
