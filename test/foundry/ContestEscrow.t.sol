066b82d697d84128720d3780df5fe51328911ab8
Viktor Estrada <vitekes@gmail.com>
2025-06-30 12:49:09 +0300

    Add additional tests and coverage docs

--- test/foundry/ContestEscrow.t.sol
+++ test/foundry/ContestEscrow.t.sol
@@ -6,6 +6,7 @@ import {ContestEscrow} from "contracts/modules/contests/ContestEscrow.sol";
 import {PrizeInfo, PrizeType} from "contracts/modules/contests/shared/PrizeInfo.sol";
 import {MockRegistry} from "contracts/mocks/MockRegistry.sol";
 import {TestToken} from "contracts/mocks/TestToken.sol";
+import {GracePeriodNotExpired} from "contracts/errors/Errors.sol";
 
 contract ContestEscrowTest is Test {
     ContestEscrow internal escrow;
@@ -17,8 +18,20 @@ contract ContestEscrowTest is Test {
     function setUp() public {
         token = new TestToken("T", "T");
         PrizeInfo[] memory prizes = new PrizeInfo[](2);
-        prizes[0] = PrizeInfo({prizeType: PrizeType.MONETARY, token: address(token), amount: 100 ether, distribution: 0, uri: ""});
-        prizes[1] = PrizeInfo({prizeType: PrizeType.MONETARY, token: address(token), amount: 50 ether, distribution: 0, uri: ""});
+        prizes[0] = PrizeInfo({
+            prizeType: PrizeType.MONETARY,
+            token: address(token),
+            amount: 100 ether,
+            distribution: 0,
+            uri: ""
+        });
+        prizes[1] = PrizeInfo({
+            prizeType: PrizeType.MONETARY,
+            token: address(token),
+            amount: 50 ether,
+            distribution: 0,
+            uri: ""
+        });
         MockRegistry reg = new MockRegistry();
         escrow = new ContestEscrow(creator, prizes, address(reg), 0, address(token), block.timestamp + 1 days);
         token.transfer(address(escrow), 150 ether);
@@ -34,5 +47,64 @@ contract ContestEscrowTest is Test {
         assertEq(token.balanceOf(w2), 50 ether);
         assertTrue(escrow.finalized());
     }
-}
 
+    function testCancelReturnsFunds() public {
+        vm.prank(creator);
+        escrow.cancel();
+        assertEq(token.balanceOf(creator), 150 ether);
+        assertTrue(escrow.finalized());
+    }
+
+    function testEmergencyWithdrawAfterGrace() public {
+        uint256 warpTo = escrow.deadline() + escrow.GRACE_PERIOD() + 1;
+        vm.warp(warpTo);
+        vm.prank(creator);
+        escrow.emergencyWithdraw();
+        assertEq(token.balanceOf(creator), 150 ether);
+        assertTrue(escrow.finalized());
+    }
+
+    function testEmergencyWithdrawBeforeGraceReverts() public {
+        vm.warp(block.timestamp + 1 days);
+        vm.prank(creator);
+        vm.expectRevert(GracePeriodNotExpired.selector);
+        escrow.emergencyWithdraw();
+    }
+
+    function testFinalizeDescendingDistribution() public {
+        PrizeInfo[] memory prizes = new PrizeInfo[](3);
+        prizes[0] = PrizeInfo({
+            prizeType: PrizeType.MONETARY,
+            token: address(token),
+            amount: 60 ether,
+            distribution: 1,
+            uri: ""
+        });
+        prizes[1] = PrizeInfo({
+            prizeType: PrizeType.MONETARY,
+            token: address(token),
+            amount: 60 ether,
+            distribution: 1,
+            uri: ""
+        });
+        prizes[2] = PrizeInfo({
+            prizeType: PrizeType.MONETARY,
+            token: address(token),
+            amount: 60 ether,
+            distribution: 1,
+            uri: ""
+        });
+        MockRegistry reg = new MockRegistry();
+        ContestEscrow esc =
+            new ContestEscrow(creator, prizes, address(reg), 0, address(token), block.timestamp + 1 days);
+        token.transfer(address(esc), 180 ether);
+        address[] memory winners = new address[](3);
+        winners[0] = w1;
+        winners[1] = w2;
+        winners[2] = address(0x3);
+        vm.prank(creator);
+        esc.finalize(winners, 0, 0);
+        assertEq(token.balanceOf(w1), 30 ether);
+        assertEq(token.balanceOf(w2), 20 ether);
+    }
+}
---
+++ test/foundry/ContestValidator.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Test.sol";
+import {AccessControlCenter} from "contracts/core/AccessControlCenter.sol";
+import {ContestValidator} from "contracts/modules/contests/ContestValidator.sol";
+import {MockValidator} from "contracts/mocks/MockValidator.sol";
+import {TestToken} from "contracts/mocks/TestToken.sol";
+import {PrizeInfo, PrizeType} from "contracts/modules/contests/shared/PrizeInfo.sol";
+import {ZeroAddress, NotAllowedToken} from "contracts/errors/Errors.sol";
+
+contract ContestValidatorTest is Test {
+    AccessControlCenter internal acc;
+    ContestValidator internal val;
+    MockValidator internal mval;
+    TestToken internal token;
+    address internal governor = address(this);
+    address internal other = address(0xBEEF);
+
+    function setUp() public {
+        acc = new AccessControlCenter();
+        acc.initialize(governor);
+        acc.grantRole(acc.GOVERNOR_ROLE(), governor);
+        mval = new MockValidator();
+        val = new ContestValidator(address(acc), address(mval));
+        token = new TestToken("T", "T");
+    }
+
+    function testValidatePrizeAllowed() public {
+        mval.setToken(address(token), true);
+        PrizeInfo memory prize =
+            PrizeInfo({prizeType: PrizeType.MONETARY, token: address(token), amount: 1 ether, distribution: 0, uri: ""});
+        val.validatePrize(prize);
+    }
+
+    function testValidatePrizeInvalidToken() public {
+        mval.setToken(address(token), false);
+        PrizeInfo memory prize =
+            PrizeInfo({prizeType: PrizeType.MONETARY, token: address(token), amount: 1 ether, distribution: 0, uri: ""});
+        vm.expectRevert(NotAllowedToken.selector);
+        val.validatePrize(prize);
+    }
+
+    function testSetTokenValidator() public {
+        MockValidator newVal = new MockValidator();
+        vm.prank(governor);
+        val.setTokenValidator(address(newVal));
+        assertEq(address(val.tokenValidator()), address(newVal));
+    }
+
+    function testSetTokenValidatorZeroReverts() public {
+        vm.prank(governor);
+        vm.expectRevert(ZeroAddress.selector);
+        val.setTokenValidator(address(0));
+    }
+}
